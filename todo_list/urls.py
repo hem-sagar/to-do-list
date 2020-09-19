41db2e560262edb68f9f92475408e60d66b14f00
HEMSAGAR ADHIKARI <hemsagar071@gmail.com>
2020-09-19 21:54:03 +0545

    url updates

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^',views.home, name="home"),
-    url(r'^delete/(?P<pk>.*)', views.delete, name="delete"),
+    url('delete/(?P<pk>.*)/', views.delete, name="delete"),
+    #url(r'^delete/(?P<pk>.*)', views.delete, name="delete"),
     url(r'^cross_off/(?P<pk>.*)', views.cross_off, name="cross_off"),
     url(r'^uncross/(?P<pk>.*)', views.uncross, name="uncross"),
     url(r'^edit/(?P<pk>.*)', views.edit, name="edit"),
